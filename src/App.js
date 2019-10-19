3aa118413f43a9926464d1b007ed059eaf948278
Derrick Sekidde <kidricederek@gmail.com>
2019-10-19 12:09:24 +0300

    feat(setup): Basic structure -Setup basic structure [finishes #1234]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containe">
+      <Container className="purchase-card">
+        <h1>Derek start</h1>
+      </Container>
     </div>
+    
   );
 }
 
